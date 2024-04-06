ba81f67df80327ab2907cc49846a893e7e195f2f
Vaibhav <Vaibhavmathur128@gmail.com>
2024-04-06 11:02:47 +0530

    Validated input for entering values only not string

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-;
+
 function UserInput() {
  const[enteredValue,setEnteredValue]= useState({
-  Amount:'15000',
-  Interest:'8.2%',
-  Duration:'12'
+  Amount:15000,
+  Interest:8.2,
+  Duration:12
  })
  function handleChange(event) {
   const { name, value } = event.target;
   setEnteredValue(prevState => ({
     ...prevState,
-    [name]: value
+    [name]: +value
   }));
+  console.log(name+" "+value);
+  
 }
 
   return (
@@ -19,16 +21,17 @@ function UserInput() {
       <div className="">
         <label>
           Amount (INR):
-          <input name="Amount" value={enteredValue.Amount} onChange={(event)=>handleChange(event)}/>
+          <input name="Amount" type="number" value={enteredValue.Amount} onChange={(event)=>handleChange(event)}/>
         </label>
         <label>
           Interest(in Percentage) :
-          <input name="Interest" value={enteredValue.Interest} onChange={(event)=>handleChange(event)}/>
+          <input name="Interest" type="number" value={enteredValue.Interest} onChange={(event)=>handleChange(event)}/>
         </label>
         <label>
           Duration (in Months):
-          <input name="Duration" value={enteredValue.Duration} onChange={(event)=>handleChange(event)}/>
+          <input name="Duration" type="number" value={enteredValue.Duration} onChange={(event)=>handleChange(event)}/>
         </label>
+      
       </div>
     </>
   );
